198a15f1c978381d1539cc7612f850e7e9bc8977
Javier Roncallo <jroncallo96@gmail.com>
2023-07-01 10:59:12 -0500

    Remove the has_and_belongs_to_many relations between the sale and product model

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,5 @@
 class Sale < ApplicationRecord
   belongs_to :customer, class_name: 'User'
   belongs_to :salesperson, class_name: 'User'
-  has_and_belongs_to_many :products
+  belongs_to :product
 end
--- db/migrate/20230701135759_create_sales.rb
+++ db/migrate/20230701135759_create_sales.rb
@@ -3,6 +3,7 @@ class CreateSales < ActiveRecord::Migration[7.0]
     create_table :sales do |t|
       t.references :customer, null: false, foreign_key: { to_table: :users }
       t.references :salesperson, null: false, foreign_key: { to_table: :users }
+      t.references :product, null: false, foreign_key: true
       t.string :city
       t.string :state
 
